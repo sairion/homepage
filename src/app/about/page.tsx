909b09cb137259b1dcd764d8c7d8e4c0f3bd308a
Jay Jaeho Lee <jay@portone.io>
2025-03-02 23:39:45 +0900

    boom

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+export default function About() {
+  return (
+    <main>
+      <h1>Jaeho Lee - Software Engineer</h1>
+      <section>Posts</section>
+      <section>
+        Links
+        <div>- GitHub - Twitter - Velog</div>
+      </section>
+      <section>Posts</section>
+      <section>- Links</section>
+      <section>- About me</section>
+    </main>
+  );
+}
